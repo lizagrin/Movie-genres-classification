e908e47539a6140b68fd318676fdcbcb57b2dfca
lizagran <liza.grankina@gmail.com>
2024-06-06 23:50:44 +0300

    fix + bot

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # Movie-genres-classification
+This project focuses on tagging movie plot synopses with relevant tags using Natural Language Processing (NLP) and machine learning techniques. The goal is to preprocess the text data, extract features, and train multiple machine learning models to predict the most relevant tags for each synopsis.
+
+You can check the operation of the model using a [telegram bot](https://t.me/movie_genres_bot)
 
 
+### Presentation:
 
 ### Dataset:
-[Kaggle](https://www.kaggle.com/datasets/PromptCloudHQ/imdb-data)
+[dataset kaggle](https://www.kaggle.com/datasets/cryptexcode/mpst-movie-plot-synopses-with-tags?select=partition.json)
+
+### Model Training
+
+Three machine learning models are trained using the preprocessed data:
+
+1. **Logistic Regression**
+2. **Support Vector Machine (SVM)**
+3. **Random Forest**
+
+Each model is tuned using GridSearchCV to find the best hyperparameters.
+
+### Metrics:
+The trained models are evaluated using F1 Score on a test set.
+
+### Results
+The results of the models are summarized in a table, showing the best parameters, F1 Score and Accuracy for each model. 
+The best-performing model is selected based on the highest F1 Score.
+
---
+++ bot.py
@@ -0,0 +1,82 @@
+import telebot
+import joblib
+import re
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords as nltk_stopwords
+from nltk.stem import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+
+TOKEN = '7386731157:AAEGchiIgeGWJbIvrHT9OjP3hf17pHzCeik'
+bot = telebot.TeleBot(TOKEN)
+
+
+def expand_contractions(text):
+    contractions = {
+        "won't": "will not",
+        "can't": "cannot",
+        "n't": " not",
+        "'re": " are",
+        "'s": " is",
+        "'d": " would",
+        "'ll": " will",
+        "'ve": " have",
+        "'m": " am"
+    }
+    for contraction, expansion in contractions.items():
+        text = re.sub(contraction, expansion, text)
+    return text
+
+
+stop_words = set(nltk_stopwords.words('english'))
+stemmer = SnowballStemmer('english')
+lemmatizer = WordNetLemmatizer()
+
+
+def process_synopsis(synopsis, stop_words, stemmer, lemmatizer):
+    synopsis = re.sub(r"http\S+", "", synopsis)
+    synopsis = BeautifulSoup(synopsis, 'lxml').get_text()
+    synopsis = expand_contractions(synopsis)
+    synopsis = re.sub(r"\S*\d\S*", "", synopsis).strip()
+    synopsis = re.sub(r'[^A-Za-z]+', ' ', synopsis)
+
+    processed_words = [
+        stemmer.stem(lemmatizer.lemmatize(word.lower())).encode('utf8')
+        for word in synopsis.split() if word.lower() not in stop_words
+    ]
+
+    return b' '.join(processed_words)
+
+
+def load_model(model_path):
+    model = joblib.load(model_path)
+    return model
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id,
+                     f'Hi, {message.from_user.first_name}! Send me a movie description and I will predict the tags.')
+
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    model = load_model('best_model.pkl')
+    vectorizer = load_model('tfidf_vectorizer.pkl')
+    mlb = load_model('mlb.pkl')
+
+    user_text = message.text
+    processed_text = process_synopsis(user_text, stop_words, stemmer, lemmatizer)
+    processed_text_decoded = processed_text.decode("utf-8")
+    vectorized_text = vectorizer.transform([processed_text_decoded])
+
+    prediction = model.predict(vectorized_text)
+    tags = mlb.inverse_transform(prediction)
+
+    tags_list = [tag for sublist in tags for tag in sublist]
+    tags_str = ', '.join(tags_list)
+
+    bot.send_message(message.chat.id, f'Predicted tags: {tags_str}')
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
